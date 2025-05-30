9fb8b0a64e249385a8868708b78a0c4a82ef191c
Kaike Sousa <kaike.cyw@gmail.com>
2025-05-30 17:49:37 -0300

    criando um menu hamburguer para responsividade do header

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// INICIO HEADER
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "src/imgs/menu.svg";
+    } else {
+        menuMobile.classList.add('open');
+        document.querySelector('.icon').src = "src/imgs/sair.png";
+    }
+}
+// FIM DO HEADER
